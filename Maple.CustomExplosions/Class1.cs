35db9637e3abc67542647b171acb921008c1ec9f
红色枫叶 <75351663@qq.com>
2019-10-10 22:26:38 +0800

    ADD

--- Maple.CustomExplosions/Class1.cs
+++ Maple.CustomExplosions/Class1.cs
@@ -2,7 +2,12 @@
 
 namespace Maple.CustomExplosions
 {
-    public class Class1
+    /// <summary>
+    /// 所有的RPC调用应该继承这个
+    /// </summary>
+    public class CRpcCallbackDTO
     {
+        public int Code { set; get; }
+        public string Messge { set; get; }
     }
 }
